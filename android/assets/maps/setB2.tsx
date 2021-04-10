bb54e50d64a7a3df2338a090278fdb2e5db13224
Alexey <50459955+moscow247@users.noreply.github.com>
2021-04-10 17:27:18 +0300

    delete something

--- android/assets/maps/setB2.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.4" tiledversion="1.4.3" name="setB2" tilewidth="16" tileheight="16" tilecount="2304" columns="48">
- <image source="setBuild2.png" width="768" height="768"/>
-</tileset>
