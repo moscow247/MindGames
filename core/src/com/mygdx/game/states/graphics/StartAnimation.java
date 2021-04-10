c7b86899700d6160e3586fd94885344ed9117b9a
Alexey <50459955+moscow247@users.noreply.github.com>
2021-04-10 18:30:12 +0300

    add new class

--- core/src/com/mygdx/game/states/graphics/StartAnimation.java
+++ core/src/com/mygdx/game/states/graphics/StartAnimation.java
@@ -1,7 +1,33 @@
 package com.mygdx.game.states.graphics;
 
 
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.mygdx.game.states.GameStateManager;
+import com.mygdx.game.states.State;
 
-public class StartAnimation {
+public class StartAnimation  extends State {
 
+    public StartAnimation(GameStateManager gms) {
+        super(gms);
+    }
+
+    @Override
+    protected void handleInput() {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+
+    @Override
+    public void update(float dt) {
+
+    }
+
+    @Override
+    public void render(SpriteBatch sb) {
+
+    }
 }
